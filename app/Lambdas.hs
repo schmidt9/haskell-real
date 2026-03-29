d5a43c24cb05b26648e4402650aa85bfa4f178d8
ANK-PC\user <kormanowskij@yandex.ru>
2026-03-29 22:03:30 +0300

    add map example;

--- app/Lambdas.hs
+++ app/Lambdas.hs
@@ -1,7 +1,15 @@
 module Lambdas where
 
+import Data.Char (toUpper)
+
 concatSquare :: Int -> String
 concatSquare x =
   r ++ " square"
   where
-    r = show ((\v -> v * v) x)
+    -- lambda with functions composition . and application operator $
+    -- same as show ((\v -> v * v) x)
+    r = show . (\v -> v * v) $ x
+
+-- higher order functions (map)
+strToUpper :: String -> String
+strToUpper = map toUpper
